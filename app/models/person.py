15a574236b491b0488a93d70db875a51fd5428af
viper23 <htonnycarlos@gmail.com>
2023-09-22 17:07:53 +0200

    added new model

--- app/models/person.py
+++ app/models/person.py
@@ -1,24 +1,29 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-from app.models import Commune, Zone, Profession, Quartier
+from app.models import Province, Commune, Zone, Profession, Quartier
 
 class Person (models.Model):
 
     relationship_with_parents = (
         ('ENFANTS','ENFANTS'),
-        ('FAMILLE', 'MEMBRE_FAMILLE'),
+        ('MEMBRE_FAMILLE', 'MEMBRE_FAMILLE'),
         ('AMIS', 'AMIS'),
         ('GROOM', 'GROOM'),
+    )
+
+    gender = (
+        ('Male', 'Male'), ('Female', 'Female')
     )
 
     nom = models.CharField(max_length=15)
     prenom = models.CharField(max_length=15)
     numero_carte_id = models.CharField(max_length=30, unique=True)
     lieu_de_delivrance = models.CharField(max_length=30)
-    commune = models.ForeignKey(Commune, on_delete=models.CASCADE)
-    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
-    quartier = models.ForeignKey(Quartier, on_delete=models.CASCADE)
+    province_de_residence = models.ForeignKey(Province, on_delete=models.CASCADE)
+    commune_de_residence = models.ForeignKey(Commune, on_delete=models.CASCADE)
+    zone_de_residence = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    quartier_de_residence = models.ForeignKey(Quartier, on_delete=models.CASCADE)
     rue = models.CharField(max_length=30)
     lieu_de_naissance = models.CharField(max_length=50)
     annee_de_naissance = models.DateField()
@@ -27,7 +32,7 @@ class Person (models.Model):
     profession = models.ForeignKey(Profession, on_delete = models.CASCADE)
     numero_telephone = PhoneNumberField(null=True)
     relation_avec_chefs_de_menage = models.CharField(choices=relationship_with_parents,max_length=20, null=True)
-    photo = models.ImageField(upload_to='photos/')
+    photo = models.ImageField(upload_to='app/photos/')
     est_chef_de_manage = models.BooleanField(default=False)
 
     def __str__(self) -> str:
