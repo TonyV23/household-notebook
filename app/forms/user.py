04cb2662620b180435af5008b8029fe85d07ba76
viper23 <htonnycarlos@gmail.com>
2023-09-25 19:56:00 +0200

    perform forms

--- app/forms/user.py
+++ app/forms/user.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
+from django import forms
 
-class UserForm(UserCreationForm):
+class UserForm(forms.ModelForm):
+    password1 = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmez le mot de passe', widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = [
@@ -12,10 +15,28 @@ class UserForm(UserCreationForm):
             'password2'
         ]
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                "Les mots de passe ne correspondent pas.")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data['password1']
+        user.set_password(password)
+        if commit:
+            user.save()
+        return user
+
+
 class UserEditInfoForm(UserChangeForm):
     class Meta:
         model = User
         fields = [
+            'username',
             'email',
-            'username'
         ]
