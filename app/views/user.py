103618d3e43382715186dbd3290a1aabfd65f4c0
viper23 <htonnycarlos@gmail.com>
2023-09-25 19:57:11 +0200

    users account views

--- app/views/user.py
+++ app/views/user.py
@@ -8,92 +8,115 @@ from django.http import HttpRequest
 
 from app.forms import UserForm, UserEditInfoForm
 
+
 def account_type(request):
     page_title = 'Type de compte'
     template = 'app/settings/account/type.html'
 
     variable = {
-        'page_title' : page_title,
+        'page_title': page_title,
     }
 
     return render(
         request,
-        template_name = template,
-        context = variable
+        template_name=template,
+        context=variable
     )
 
-def index(request) :
-    page_title = 'Types de comptes'
-    users = User.objects.all()
-    template = 'app/settings/users/index.html'
+def index_chef_family(request):
+    page_title = 'Comptes des chefs de famille'
+    group = Group.objects.get(name="chef_family")
+    users = User.objects.filter(groups=group).order_by('username')
+    template = 'app/settings/account/index_chef_family.html'
 
     variable = {
-        'page_title' : page_title,
-        'users' : users
+        'page_title': page_title,
+        'users': users,
+        'group': group
     }
 
     return render(
         request,
-        template_name = template,
-        context = variable
+        template_name=template,
+        context=variable
     )
 
-def add_user(request) :
-    page_title = 'Nouveau compte'
-    template = 'app/settings/users/add.html'
+def index_chef_quarter(request):
+    page_title = 'Comptes des chefs de quartiers'
+    group = Group.objects.get(name="chef_quarter")
+    users = User.objects.filter(groups=group).order_by('username')
+    template = 'app/settings/account/index_chef_quarter.html'
+
+    variable = {
+        'page_title': page_title,
+        'users': users
+    }
+
+    return render(
+        request,
+        template_name=template,
+        context=variable
+    )
+
+def add_user_chef_family(request):
+    page_title = 'Compte du chef de menage'
+    template = 'app/settings/account/add_chef_family.html'
     form = UserForm()
 
     variable = {
-        'page_title' : page_title,
+        'page_title': page_title,
         'form': form
     }
-    
+
     return render(
-        request, 
-        template_name = template,
-        context = variable
+        request,
+        template_name=template,
+        context=variable
     )
 
-def store_user(request):
+def add_user_chef_quarter(request):
+    page_title = 'Compte du chef de quartier'
+    template = 'app/settings/account/add_chef_quarter.html'
+    form = UserForm()
+
+    variable = {
+        'page_title': page_title,
+        'form': form
+    }
+
+    return render(
+        request,
+        template_name=template,
+        context=variable
+    )
+
+def store_user_chef_family(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            group = Group.objects.get(name = 'students')
-            user.groups.add(group)    
+            group = Group.objects.get(name='chef_family')
+            user.groups.add(group)
             messages.success(request, 'Le compte de '+username+' a été ajouté')
-        
-        return redirect('/accounts')
 
-def login_user(request) :
+        return redirect('/account/list_chef_family')
+
+def store_user_chef_quarter(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            messages.info(request, 'Nom d\'utilisateur ou mot de passe incorrect')
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            group = Group.objects.get(name='chef_quarter')
+            user.groups.add(group)
+            messages.success(request, 'Le compte de '+username+' a été ajouté')
 
-    page_title = 'Se connecter'
-    template  = 'app/settings/users/login.html'
-    
-    variable = {
-        'page_title' : page_title
-    }
+        return redirect('/account/list_chef_family')
 
-    return render(
-        request,
-        template_name = template,
-        context = variable
-    )
-
-def edit_user(request, id) :
+def edit_user(request, id):
     assert isinstance(request, HttpRequest)
-    page_title = 'Modifier les informations de l\'étudiant'
+    page_title = 'Modifier les données de l\'utilisateur'
     if request.method == 'GET':
         if id == 0:
             form = UserEditInfoForm()
@@ -101,18 +124,18 @@ def edit_user(request, id) :
             user = User.objects.get(pk=id)
             form = UserEditInfoForm(instance=user)
 
-        template = "app/settings/users/edit.html"
+        template = "app/settings/account/edit.html"
         context = {
-            'page_title' : page_title,
-            'form' : form
+            'page_title': page_title,
+            'form': form
         }
         return render(
             request,
-            template_name = template,
-            context= context
+            template_name=template,
+            context=context
         )
 
-def update_user(request, id) :
+def update_user(request, id):
     if request.method == 'POST':
         if id == 0:
             form = UserEditInfoForm(request.POST)
@@ -121,16 +144,22 @@ def update_user(request, id) :
             form = UserEditInfoForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-        messages.success(request, "Les informations de l'étudiant ont été modifié avec succès !")
-        return redirect('/accounts') 
+            groups = user.groups.all()
+            if groups.filter(name='chef_family').exists():
+                return redirect('/account/list_chef_family')
+            elif groups.filter(name='chef_quarter').exists():
+                return redirect('/account/list_chef_quarter')
+        messages.success(request, "Les données ont été modifié !")
+        return redirect('/type_account')
 
-def logout_user(request) :
-    logout(request)
-
-    return redirect('/login')
-
-def delete_user(request, id) :
-    user = User.objects.get(pk = id)
+def delete_user(request, id):
+    user = User.objects.get(pk=id)
     user.delete()
-    messages.success(request, 'Le compte a été supprimé avec succès')
-    return redirect('/accounts')
+    groups = user.groups.all()
+    if groups.filter(name='chef_family').exists():
+        return redirect('/account/list_chef_family')
+    elif groups.filter(name='chef_quarter').exists():
+        return redirect('/account/list_chef_quarter')
+    messages.success(request, 'Le compte a été supprimé')
+    return redirect('/type_account')
+
