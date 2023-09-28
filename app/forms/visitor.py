0575fc7bd427a3a90edc975ca8c773b8f47ac6d5
viper23 <htonnycarlos@gmail.com>
2023-09-28 19:14:17 +0200

    updated form

--- app/forms/visitor.py
+++ app/forms/visitor.py
@@ -7,7 +7,8 @@ from app.models import Visitor
 class VisitorForm(ModelForm):
     class Meta:
         model = Visitor
-        exclude = ['created_at', 'created_by', 'date_arrive']
+        exclude = ['created_at', 'created_by', 'est_chef_de_menage','date_arrive']
         widgets = {
+            'annee_de_naissance': forms.DateInput(attrs={'type': 'date'}),
             'date_depart': forms.DateInput(attrs={'type': 'date'}),
         }
