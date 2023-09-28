3b43f3f35774b70b00de0c4b3824dbd23931aa71
viper23 <htonnycarlos@gmail.com>
2023-09-28 09:36:04 +0200

    exclude created_at field

--- app/forms/visitor.py
+++ app/forms/visitor.py
@@ -7,8 +7,7 @@ from app.models import Visitor
 class VisitorForm(ModelForm):
     class Meta:
         model = Visitor
-        exclude = ['date_arrive']
+        exclude = ['created_at', 'created_by', 'date_arrive']
         widgets = {
             'date_depart': forms.DateInput(attrs={'type': 'date'}),
         }
-        fields = '__all__'
