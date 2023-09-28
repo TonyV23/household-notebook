94cb2d0cdd66f7cdce8b1c4e0e9f3f6db4c6db75
viper23 <htonnycarlos@gmail.com>
2023-09-28 19:29:40 +0200

    request for total household

--- app/views/home.py
+++ app/views/home.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-
+from app.models import Household
 @login_required(login_url ='login')
 def index (request) :
     page_title = 'Accueil'
     template = 'app/home/index.html'
+
+    total_households = Household.objects.all().count()
+
+
     context = {
         'page_title' : page_title,
-        'template' : template
+        'template' : template,
+        'total_households' : total_households,
     }
     
     return render(request, template_name=template, context=context)
