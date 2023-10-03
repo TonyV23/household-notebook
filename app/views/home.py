704de34c69423b07637d76d2596ef883a120bf5a
viper23 <htonnycarlos@gmail.com>
2023-10-03 13:18:38 +0200

    calculation of total persons

--- app/views/home.py
+++ app/views/home.py
@@ -2,19 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from app.models import Household
+from app.models import Household, Person, Visitor
 @login_required(login_url ='login')
 def index (request) :
     page_title = 'Accueil'
     template = 'app/home/index.html'
 
     total_households = Household.objects.all().count()
+    total_member_family = Person.objects.all().count()
+    total_visitors = Visitor.objects.all().count()
 
+    total_persons = total_member_family+total_visitors
 
     context = {
         'page_title' : page_title,
         'template' : template,
         'total_households' : total_households,
+        'total_persons' : total_persons,
     }
     
     return render(request, template_name=template, context=context)
