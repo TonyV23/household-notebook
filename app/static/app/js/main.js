740b5c2009c9cec04c635be9c3a3aeb048e98696
viper23 <htonnycarlos@gmail.com>
2023-10-06 09:20:54 +0200

    working on sync adresse

--- app/static/app/js/main.js
+++ app/static/app/js/main.js
@@ -1,16 +1,26 @@
-/**
-* Template Name: NiceAdmin
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+// SYNC PROVINCE COMMUNE ZONE QUARTIER
+
+$(function () {
+  $("#id_province").on("change", function () {
+    id_province = $(this).val();
+    //alert(id_province);
+    $.get(
+      "/distributions/getCommunes",
+      {
+        id_province: id_province,
+      },
+      function (data, textStatus, jqXHR) {
+        $("#id_commune").html(data);
+      }
+    );
+  });
+}); 
+
+
+
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -20,9 +30,6 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
   const on = (type, el, listener, all = false) => {
     if (all) {
       select(el, all).forEach(e => e.addEventListener(type, listener))
@@ -31,34 +38,25 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Sidebar toggle
-   */
+  
   if (select('.toggle-sidebar-btn')) {
     on('click', '.toggle-sidebar-btn', function(e) {
       select('body').classList.toggle('toggle-sidebar')
     })
   }
 
-  /**
-   * Search bar toggle
-   */
+  
   if (select('.search-bar-toggle')) {
     on('click', '.search-bar-toggle', function(e) {
       select('.search-bar').classList.toggle('search-bar-show')
     })
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
