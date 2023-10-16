3e9edd5cb33d5d015382e75c51e144e91263096a
viper23 <htonnycarlos@gmail.com>
2023-10-16 15:40:11 +0200

    changed person model structure

--- app/models/person.py
+++ app/models/person.py
@@ -20,22 +20,22 @@ class Person (models.Model):
     commune_de_residence = models.ForeignKey(Commune, on_delete=models.PROTECT)
     zone_de_residence = models.ForeignKey(Zone, on_delete=models.PROTECT)
     quartier_de_residence = models.ForeignKey(Quartier, on_delete=models.PROTECT)
-    rue = models.CharField(max_length=30, blank=True)
+    rue = models.CharField(max_length=30, null=True, blank=True)
     lieu_de_naissance = models.CharField(max_length=50)
     annee_de_naissance = models.DateField()
     nom_du_pere = models.CharField(max_length=30)
     nom_de_la_mere = models.CharField(max_length=30)
     profession = models.ForeignKey(Profession, on_delete=models.PROTECT)
-    numero_telephone = models.CharField(max_length=15, blank=True)
+    numero_telephone = models.CharField(max_length=15, null=True, blank=True)
     relation_avec_chefs_de_menage = models.ForeignKey(Status, on_delete=models.PROTECT)
-    photo = models.ImageField(upload_to='photos/', blank=True)
+    photo = models.ImageField(upload_to='photos/', null=True, blank=True)
 
     est_chef_de_menage = models.BooleanField(default=False)
     est_verifie_par_chef_de_menage = models.BooleanField(default=False)
     est_verifie_par_chef_de_quartier = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    date_depart = models.DateTimeField(blank=True)
+    date_depart = models.DateTimeField(null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
 
     def __str__(self) -> str:
