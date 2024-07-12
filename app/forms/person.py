c044c171a3d183501518bb58022505f606826b40
viper23 <htonnycarlos@gmail.com>
2024-07-12 12:59:20 +0200

    debug

--- app/forms/person.py
+++ app/forms/person.py
@@ -1,10 +1,9 @@
 from django.forms import ModelForm
 from django import forms
-
-
 from app.models import Person
-class PersonForm (ModelForm):
-     class Meta:
+
+class PersonForm(ModelForm):
+    class Meta:
         model = Person
         exclude = ['created_at', 'created_by']
         widgets = {
--- app/models/person.py
+++ app/models/person.py
@@ -10,11 +10,7 @@ class Person (models.Model):
     gender = (
         ('Male', 'Male'), ('Female', 'Female')
     )
-
-    is_verified = (
-        (1, 'Oui'),
-        (0, 'Non')
-    )
+    
     menage = models.ForeignKey(Household, on_delete=models.CASCADE)
     nom = models.CharField(max_length=15)
     prenom = models.CharField(max_length=15)
@@ -35,10 +31,9 @@ class Person (models.Model):
     relation_avec_chefs_de_menage = models.ForeignKey(Status, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='photos/', null=True, blank=True)
 
-
-    est_chef_de_menage = models.IntegerField(choices=is_verified, null=True, blank=True)
-    est_verifie_par_chef_de_menage = models.IntegerField(choices=is_verified, null=True, blank=True)
-    est_verifie_par_chef_de_quartier = models.IntegerField(choices=is_verified, null=True, blank=True)
+    est_chef_de_menage = models.BooleanField(default=False)
+    est_verifie_par_chef_de_menage = models.BooleanField(default=False)
+    est_verifie_par_chef_de_quartier = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     date_depart = models.DateField(null=True, blank=True)
